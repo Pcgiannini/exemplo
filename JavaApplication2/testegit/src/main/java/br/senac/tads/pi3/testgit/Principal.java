17364945d0c2cb189c4bfbe2e1c4bfc72a0ac51d
paulo.cgiannini1 <paulo.cgiannini1@CAS511774W0718.senacsp.edu.br>
2017-02-21 20:36:15 -0300

    Versão Incial

---
+++ JavaApplication2/testegit/src/main/java/br/senac/tads/pi3/testgit/Principal.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.tads.pi3.testgit;
+
+/**
+ *
+ * @author paulo.cgiannini1
+ */
+public class Principal {
+    
+
+public static void main (string[]args) {
+    System.out.println ("Hello World");
+}
+
+}
